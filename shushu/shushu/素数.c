7bcd1c489deab5f756f55cc3d446922a56b7338b
ll <2847084717@qq.com>
2019-09-29 21:41:37 +0800

    提交代码

---
+++ shushu/shushu/素数.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	printf("输出100到200之间的素数：\n");
+
+	int n,m ;
+	
+	for (n = 100; n <= 200;n++)
+	{
+		for (m = 2; m < n;m++)
+		if (n%m == 0)
+			break;
+		if (n>=m)
+		printf("%d\n", n);
+	}
+	
+	system("pause");
+ }
