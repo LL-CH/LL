0989a77bd8c8bfeba3703e6fc7732543c5b6a6da
ll <2847084717@qq.com>
2019-09-29 22:35:18 +0800

    数组交换

--- 数组交换.cpp
+++ 数组交换.cpp
@@ -1,18 +1,17 @@
 
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int exchange(int arr1[], int arr2[], int size){
+int exchange(int arr1[], int arr2[], int size){   //定义一个交换数值的函数//
 	int i = 0;
 
 	for (i = 0; i <= size; i++){
-	
+
 		int num = arr1[i];
 		arr1[i] = arr2[i];
 		arr2[i] = num;
 
-
-	}return size;
+	}
+	return size;
 }
 
 int main()
@@ -20,14 +19,15 @@ int main()
 	int i = 0;
 	int arr1[10] = { 1, 2, 3, 6, 5, 4, 8, 7, 9, 8 };
 	int arr2[10] = { 9, 8, 5, 4, 7, 6, 1, 2, 0, 3 };
-	exchange(arr1, arr2, sizeof(arr1) / sizeof(arr1[0]));
+	exchange(arr1, arr2, sizeof(arr1) / sizeof(arr1[0]));    //代入函数//
 	for (i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++){
 		printf("%d", arr1[i]);
+	
+	}printf("\n");
+	for (i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++){
 		printf("%d", arr2[i]);
-	}return(0);
 
-	
-	
+	}printf("\n");
 	system("pause");
 
 }
