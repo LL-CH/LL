4e66c4cd6d1b997b0b35d75792d83555c9994780
ll <2847084717@qq.com>
2019-10-11 20:33:37 +0800

    修改

--- 公约数.c
+++ 公约数.c
@@ -5,20 +5,36 @@
 int main()
 {
 	int a, b;
+	int min,i;
 		printf("输入两个数字:\n");
 		scanf("%d%d", &a, &b);
-		while (a != b)
-		{
-			if (a > b)
-				a = a - b;
-			else
-				b = b - a;
-			
+		min = a;
+		for (i = min; i > 0; i--){
+			if (a%i == 0 && b%i == 0){
+				printf("最大公约数是:\n",i);
+				break;
+			}return 1;
 		}
-		printf("最大公约数是:\n");
-		printf("%d\n", a);
-		
-	
+		/*	while (a != b)
+			{
+			if (a > b)
+			a = a - b;
+			else
+			b = b - a;
+
+			}
+			printf("最大公约数是:\n");
+			printf("%d\n", a);
+
+			*/
+	/*	residual = a%b;        //碾转相除法
+		while (residual){
+			a = b;
+			b = residual;
+			residual = a%b;
+		}
+		printf("最大公约数是:\n",b);
+		return 0;*/
 	system("pause");
 	
 }
