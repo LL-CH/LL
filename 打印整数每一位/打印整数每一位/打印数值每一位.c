aa23b16a63b41a1bccd6677e218e42a12d85e406
ll <2847084717@qq.com>
2019-10-15 20:51:25 +0800

    1

---
+++ 打印整数每一位/打印整数每一位/打印数值每一位.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+void print(int n){
+	if (n > 9){
+		print(n / 10); 
+	}
+	printf("%d ", n % 10);
+}
+int main(){
+	int num = 0;
+	printf ("输入一个数:");
+	scanf("%d", &num);
+	print (num);
+	system("pause");
+}
