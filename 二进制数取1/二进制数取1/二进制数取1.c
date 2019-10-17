8add97eeb69c10e26864073915d25194ea1f6e39
ll <2847084717@qq.com>
2019-10-17 20:43:26 +0800

    1

---
+++ 二进制数取1/二进制数取1/二进制数取1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int count_one_bits(unsigned int value)
+{
+	int count = 0;
+	while (value)
+	{
+		if (value % 2 == 1)        // 取数字的二进制序列中的一个 1 
+		{
+			count++;
+		}
+		value /= 2;                //减掉所得的一个1
+	}
+	return count;
+}
+int main()
+{
+	int num = 0;
+	printf("请输入一个数：");
+	scanf("%d", &num);
+	int count = count_one_bits(num);
+	printf("二进制序列中1的个数count = %d\n", count);
+	system("pause");
+}
