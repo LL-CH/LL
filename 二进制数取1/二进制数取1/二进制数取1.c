f08bd6096b4deb888ae6fa6d0d21c6831210aa94
ll <2847084717@qq.com>
2019-10-23 23:21:26 +0800

    2

--- 二进制数取1/二进制数取1/二进制数取1.c
+++ 二进制数取1/二进制数取1/二进制数取1.c
@@ -2,25 +2,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int count_one_bits(unsigned int value)
-{
-	int count = 0;
-	while (value)
-	{
-		if (value % 2 == 1)        // 取数字的二进制序列中的一个 1 
-		{
-			count++;
-		}
-		value /= 2;                //减掉所得的一个1
-	}
-	return count;
-}
+//int count_one_bits(unsigned int value)
+//{
+	//	int count = 0;
+	//	while (value)
+	//	{
+	//		if (value % 2 == 1)        // 取数字的二进制序列中的一个 1 
+	//		{
+	//			count++;
+	//		}
+	//		value /= 2;                //减掉所得的一个1
+	//	}
+	//	return count;
+	//}
+
+
 int main()
 {
 	int num = 0;
+	int count = 0;
 	printf("请输入一个数：");
 	scanf("%d", &num);
-	int count = count_one_bits(num);
+	/*int count = count_one_bits(num);*/
+	for (int i = 0; i < 32; i++){
+		if (((num >> i) & 1) == 1){
+			count++;
+		}
+	}
 	printf("二进制序列中1的个数count = %d\n", count);
 	system("pause");
+	return 0;
 }
