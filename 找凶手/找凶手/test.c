93c0fe045c4e6d5af948f01b0b5703425b7032ed
ll <2847084717@qq.com>
2019-10-30 22:40:14 +0800

    1

---
+++ 找凶手/找凶手/test.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>    
+#include <stdlib.h>
+
+int main()
+{
+	int murder;
+	for (murder = 'A'; murder <= 'D'; murder++)
+	{
+		//有三个人说真话一个人说假假，所以在if语句的，如果括号里为真结果为1，要是假结果为0。所以关系若成立，一定有一个说假话为0，其余为1,即结果等于3.
+		if (((murder != 'A') + (murder == 'C') + (murder == 'D') + (murder != 'D')) == 3)      
+		{
+			printf("凶手是 %c\n", murder);    
+
+		}
+	}
+	system("pause");
+}
---
+++ 计算器/计算器/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	float a, b;
+	char c;
+	printf("输入:");
+	scanf("%f %c %f", &a, &c, &b);
+	switch (c)
+	{
+		case '+':printf("%f &c &f=%f\n", a, c, b, a + b);
+			break;
+		case'-':printf("%f &c &f=%f\n", a, c, b, a - b);
+			break;
+		case'*':printf("%f &c &f=%f\n", a, c, b, a * b);
+			break;
+		case'/':
+			if (b == 0){
+				printf("输入错误!请从新输入!\n");
+			}
+			else {
+				printf("%f &c &f=%f\n", a, c, b, a / b);
+				break;
+			}
+		default:printf("输入错误!\n");
+	
+	}
+	system("pause");
+}
