9aa15dd9fd361d5ea4a946dda98335354227cd72
ll <2847084717@qq.com>
2019-10-31 22:27:11 +0800

    正确

--- 计算器/计算器/test.c
+++ 计算器/计算器/test.c
@@ -5,26 +5,30 @@
 void main(){
 	float a, b;
 	char c;
-	printf("输入:");
-	scanf("%f %c %f", &a, &c, &b);
-	switch (c)
-	{
-		case '+':printf("%f &c &f=%f\n", a, c, b, a + b);
+		printf("输入:");
+		scanf("%f%c%f", &a, &c, &b);
+
+		switch (c)
+		{
+		case '+':printf("%f%c%f=%f\n", a, c, b, a + b);
 			break;
-		case'-':printf("%f &c &f=%f\n", a, c, b, a - b);
+		case'-':printf("%f%c%f=%f\n", a, c, b, a - b);
 			break;
-		case'*':printf("%f &c &f=%f\n", a, c, b, a * b);
+		case'*':printf("%f%c%f=%f\n", a, c, b, a * b);
 			break;
 		case'/':
+			//除法分母 不能为0
 			if (b == 0){
 				printf("输入错误!请从新输入!\n");
+				break;
 			}
 			else {
-				printf("%f &c &f=%f\n", a, c, b, a / b);
+				printf("%f%c%f=%f\n", a, c, b, a / b);
 				break;
 			}
 		default:printf("输入错误!\n");
-	
+
+		
 	}
 	system("pause");
 }
