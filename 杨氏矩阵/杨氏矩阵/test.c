fcc92ab282d57518dea7d4b469fc9f9f95dee076
ll <2847084717@qq.com>
2019-11-09 21:08:00 +0800

    2

--- 杨氏矩阵/杨氏矩阵/test.c
+++ 杨氏矩阵/杨氏矩阵/test.c
@@ -1,32 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-#define ROW 3
-#define COL 3
-
-int find(int arr[ROW][COL], int n){
-	//直接将数组中的所有元素找一遍
-		for (int row = 0; row < ROW; row++){
-			for (int col = 0; col < COL; col++){
-				if (arr[row][col] == n){
-					return 1;
-					break;
-				}
-			}
-		}
-	return 0;
-}
-
-int main(){
-	int arr[ROW][COL] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int n = 10;
-	if (find(arr, n) == 1){
-		printf("找到了!\n");
-	}
-	else printf("没找到!\n");
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//#define ROW 3
+//#define COL 3
+//
+//int find(int arr[ROW][COL], int n){
+//	//直接将数组中的所有元素找一遍
+//		for (int row = 0; row < ROW; row++){
+//			for (int col = 0; col < COL; col++){
+//				if (arr[row][col] == n){
+//					return 1;
+//					break;
+//				}
+//			}
+//		}
+//	return 0;
+//}
+//
+//int main(){
+//	int arr[ROW][COL] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int n = 10;
+//	if (find(arr, n) == 1){
+//		printf("找到了!\n");
+//	}
+//	else printf("没找到!\n");
+//	system("pause");
+//	return 0;
+//}
 
 //方法二
 //#include<stdio.h>
--- 杨氏矩阵/杨氏矩阵/test2.c
+++ 杨氏矩阵/杨氏矩阵/test2.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -5,9 +6,11 @@
 #define COL 3
 
 int find(int arr[ROW][COL], int n){
+	//二维数组  int** arr
+	//从右上角的数字开始找 也可以从左下角数开始找 (ROW-1,0)
+	//时间复杂度 ROW+COL
 	int row = 0;
 	int col = COL - 1;
-	//从右上角的数字开始找
 	while (1){
 		//大了,进入下一行
 		if (arr[row][col] < n){
@@ -19,6 +22,7 @@ int find(int arr[ROW][COL], int n){
 		}
 		if (arr[row][col] == n){
 			printf("找到了!\n");
+			printf("坐标为:%d %d", row, col);
 			break;
 		}
 		else{
@@ -30,8 +34,21 @@ int find(int arr[ROW][COL], int n){
 }
 
 int main(){
+	//二维数组动态创建
+	/*int row=ROW;
+	int col=COL;
+	int** arr = (int**)malloc(sizeof(int*)*row);
+	for (int i = 0; i < row; i++){
+		arr[i] = (int*)malloc(sizeof(int)*col);
+		for (int j = 0; j < col; j++){
+			scanf("%d", &arr[row][col]);
+		}
+	}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+*/
 	int arr[ROW][COL] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int n = 10;
+	int n = 0;
+	printf("输入要找的数:");
+	scanf("%d", &n);
 	find(arr, n);
 	system("pause");
 	return 0;
