c9b2edd26c6ba14ddf80fd1592dd84b52072954c
ll <2847084717@qq.com>
2019-11-15 21:06:37 +0800

    1

---
+++ memmove/mommove/tset.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void* Memmove(void* dst, const void* src, size_t num)
+{
+	assert(src && dst);
+
+	char* d = (char*)dst;
+	char* s = (char*)src;
+	if (s < d && dst < s + num) //后重叠时从后往前拷贝
+	{
+		for (int i = num - 1; i >= 0; --i)
+		{
+			d[i] = s[i];
+		}
+	}
+	else  // 前重叠和不重叠时从前往后考
+	{
+		for (int i = 0; i < num; ++i)
+		{
+			d[i] = s[i];
+		}
+	}
+
+	return dst;
+}
+
+int main(){
+		int a2[10] = { 1, 2, 3, 4, 5 };
+		int a1[10];
+		Memmove(a1, a2, 10 * sizeof(int));
+		for (int i = 0; i < 10; ++i)
+		{
+			printf("%d ", a2[i]);
+		}
+		printf("\n");
+	system("pause");
+	return 0;
+}
