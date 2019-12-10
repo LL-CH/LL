6dc7d014997074f57f862d3121fa5a9c4b93323d
ll <2847084717@qq.com>
2019-12-10 22:48:44 +0800

    1

---
+++ 替换空格/替换空格/源.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+#include<string.h>
+
+
+void replaceSpace(char *str, int length) {            // 计算空格的个数           
+ int space_num = 0;
+ for(int i = 0; i < length; ++i)
+ { 
+	 if(str[i] == ' ')
+		 space_num++;
+ } 
+ // 从后往前，依次挪动字符，遇到空格替换为%20
+ for(int i = length-1; i >= 0; --i)
+ { 
+	 if(str[i] == ' ')
+	 { 
+		 space_num--;
+		 str[i+space_num*2] = '%';
+		 str[i+space_num*2+1] = '2';
+		 str[i+space_num*2+2] = '0'; 
+	 }
+	 else
+	 { 
+		 str[i + space_num*2] = str[i];
+	 }
+ }
+}
+
+int main(){
+	char* str = "we are happy";
+	int len = strlen(str);
+	replaceSpace(str, len);
+	printf("%s\n",str);
+
+	system("pause");
+}
