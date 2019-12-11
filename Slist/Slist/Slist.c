76ce37aa5a9c708fb21a6e4501410a30c09e365e
ll <2847084717@qq.com>
2019-12-11 20:00:53 +0800

    end

--- Slist/Slist/Slist.c
+++ Slist/Slist/Slist.c
@@ -101,3 +101,57 @@ void SListPopFront(SListNode** ppList){
 		free(cur);
 	}
 }
+
+SListNode* SListFind(SListNode* pList, SLTDataType x)
+{
+	SListNode* cur = pList;
+	while (cur)
+	{
+		if (cur->data == x)
+		{
+			return cur;
+		}
+		else
+		{
+			cur = cur->next;
+		}
+	}
+
+	return NULL;
+}
+
+
+void SListInsertAfter(SListNode* pos, SLTDataType x)
+{
+	/*SListNode* newnode = BuySListNode(x);
+	SListNode* next = pos->next;
+	pos->next = newnode;
+	newnode->next = next;*/
+
+	SListNode* newnode = BuySListNode(x);
+	pos->next = newnode;
+	newnode->next = pos->next;
+}
+
+void SListEraseAfter(SListNode* pos)
+{
+	SListNode* next = pos->next;
+	if (next != NULL)
+	{
+		pos->next = next->next;
+		free(next);
+	}
+}
+
+
+void SListDestory(SListNode** pplist)
+{
+	SListNode* cur = *pplist;
+	while (cur)
+	{
+		SListNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+	*pplist = NULL;
+}
--- Slist/Slist/Slist.h
+++ Slist/Slist/Slist.h
@@ -21,3 +21,12 @@ void SListPushFront(SListNode** pList, SLTDataType x);
 void SListPopBack(SListNode** ppList);
 //头删
 void SListPopFront(SListNode** pList);
+// 单链表查找
+SListNode* SListFind(SListNode* plist, SLTDataType x);
+// 单链表在pos位置之后插入
+void SListInsertAfter(SListNode* pos, SLTDataType x);
+// 单链表删除pos位置之后的值
+void SListEraseAfter(SListNode* pos);
+
+
+void SListDestory(SListNode** pplist);
