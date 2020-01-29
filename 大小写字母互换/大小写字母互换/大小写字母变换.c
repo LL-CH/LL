d138c969a0948f1a113a38979fea3db6c4bafa86
ll <2847084717@qq.com>
2020-01-29 21:11:07 +0800

    +

--- 大小写字母互换/大小写字母互换/大小写字母变换.c
+++ 大小写字母互换/大小写字母互换/大小写字母变换.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//字符串大写字母转为小写字母
+char * tolowercase(char * str){
+
+	for (int i = 0; i<strlen(str); i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z'){
+			str[i] = str[i] + 32;
+		}
+	}
+	return str;
+}
+
 int main()
 {
 	printf("输入一个字符:\n");
