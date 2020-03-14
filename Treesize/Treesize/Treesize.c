9144db368241c1af300ec1e2b626ad1300169325
ll <2847084717@qq.com>
2020-03-14 20:32:59 +0800

    二叉树

--- Treesize/Treesize/Treesize.c
+++ Treesize/Treesize/Treesize.c
@@ -7,8 +7,12 @@ BTNode* BinaryTreeCreate(BTDataType* a, int n, int* pi){
 }
 
 // 二叉树销毁
-void BinaryTreeDestory(BTNode** root){
-
+void BinaryTreeDestory(BTNode* root){
+	if (root == NULL)
+		return;
+	BinaryTreeDestory(root->left);
+	BinaryTreeDestory(root->right);
+	free(root);
 }
 
 // 二叉树节点个数
@@ -39,22 +43,51 @@ int BinaryTreeLevelKSize(BTNode* root, int k){
 
 // 二叉树查找值为x的节点
 BTNode* BinaryTreeFind(BTNode* root, BTDataType x){
-
+	if (root == NULL)
+		return;
+	if (root->_data == x)
+		return root;
+	//不等于时找左树
+	BTNode* ret = BinaryTreeFind(root->left, x);
+	if (ret){
+		return ret;
+	}
+	//左树没找到,找右树
+	ret = BinaryTreeFind(root->right, x);
+	if (ret){
+		return ret;
+	}
+	return NULL;
 }
 
 // 二叉树前序遍历
 void BinaryTreePrevOrder(BTNode* root){
-
+	if (root == NULL)
+		return;
+	//根->左->右
+	printf("%d ", root->_data);
+	BinaryTreePrevOrder(root->left);
+	BinaryTreePrevOrder(root->right);
 }
 
 // 二叉树中序遍历
 void BinaryTreeInOrder(BTNode* root){
-
+	if (root == NULL)
+		return;
+	//左->根->右
+	BinaryTreeInOrder(root->left);
+	printf("%d ", root->_data);
+	BinaryTreeInOrderr(root->right);
 }
 
 // 二叉树后序遍历
 void BinaryTreePostOrder(BTNode* root){
-
+	if (root == NULL)
+		return;
+	//左->右-根
+	BinaryTreePostOrder(root->left);
+	BinaryTreePostOrder(root->right);
+	printf("%d ", root->_data);
 }
 
 // 层序遍历
