401c49c4f5e8ac6587c3a8eb7b4eb350ccc57c74
ll <2847084717@qq.com>
2020-07-29 08:56:16 +0800

    剑指offer 27

---
+++ 二叉树的镜像/二叉树的镜像/test.cpp
@@ -0,0 +1,27 @@
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+	TreeNode* mirrorTree(TreeNode* root) {
+		if (root == nullptr)
+			return root;
+		if (root->left == nullptr && root->right == nullptr)
+			return root;
+		TreeNode* temp = root->left;
+		root->left = root->right;
+		root->right = temp;
+		if (root->left)
+			mirrorTree(root->left);
+		if (root->right)
+			mirrorTree(root->right);
+
+		return root;
+
+	}
+};
